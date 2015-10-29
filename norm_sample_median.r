0e44e4e9313dd99d4c4aee77c420e6324959f53f
Jorrit Boekel <jorrit.boekel@scilifelab.se>
2015-10-29 11:30:25 +0100

    Norm sample median normalizes PSM ratios always now

--- norm_sample_median.r
+++ norm_sample_median.r
@@ -1,28 +1,43 @@
-#Function for normalising to same sample median
-#Lina Hultin-Rosenberg 20120222
+################
+divide_column = function(numerator, denominator){
+  divided = numerator / denominator
+  return(divided)
+  }
 
-norm.sample.median = function(filename, quant.index)
+
+norm.sample.median = function(filename, quant.min, quant.index, denominator)
 {
   
   #Load peptide data
   pep.data = read.delim(filename,header=TRUE,check.names=FALSE,row.names=NULL,sep="\t")
-  
-  #Extract quantitative data
-  int = pep.data[,quant.index]
-  
-  int.norm = int
+
+  # Set NA to channel if below noise level  
+  pep.quant = pep.data[, quant.index] 
+  pep.quant[pep.quant < quant.min] = NA
+  pep.data[, quant.index] = pep.quant
+
+  # Define denominator column
+  if (length(denominator)==1) {
+    quant.den = pep.data[,denominator]
+  } else {
+    quant.den = apply(cbind(pep.data[,denominator]),1,mean,na.rm=TRUE)  
+  }
+   
+  #Extract quantitative data and ratios
+  ratios = apply(pep.data[, quant.index], 2, divide_column, denominator=quant.den)
+  norm_ratios = ratios
   sample.median = c()
-  for (i in 1:ncol(int)) {
-    sample.median[i] = median(int[,i],na.rm=TRUE)
+  for (i in 1:ncol(ratios)) {
+    sample.median[i] = median(ratios[,i],na.rm=TRUE)
   }
-  median.mean = mean(sample.median)
-  for (i in 1:ncol(int)) {
-    int.norm[,i] = (int[,i]/sample.median[i])*median.mean
+  #median.mean = mean(sample.median)
+  for (i in 1:ncol(ratios)) {
+    norm_ratios[, i] = (ratios[, i]/sample.median[i])
   }
   
-  #Save normalised peptide data
+  #Return normalised PSM table
   pep.norm = pep.data
-  pep.norm[,quant.index] = int.norm
+  pep.norm[,quant.index] = norm_ratios
   
   return(pep.norm)
 }
